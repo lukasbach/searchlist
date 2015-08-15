ced019a6a06a55906a4b9a8789ba31676d87b210
Lukas Bach <lbach@outlook.de>
2015-08-15 02:57:45 +0200

    Added search function

--- searchlist.js
+++ searchlist.js
@@ -1,18 +1,17 @@
-/*
- *    Allowed options:
- *      "source": Path to the json file that's being used for the list
- *        e.g. "path/to/file.json"
- *      "context": JSON path to the list
- *        e.g. "json.list.path
- *        for the list {json:{list:{path:[listel,listlel,listel]}}}
- */
-
 $.fn.searchlist = function (functionname, option) {
   return this.each(function() {
     return window[functionname](option, this);
   });
 }; // end prototype
 
+
+ /*    Allowed options:
+ *      "source": Path to the json file that's being used for the list
+ *        e.g. "path/to/file.json"
+ *      "context": JSON path to the list
+ *        e.g. "json.list.path
+ *        for the list {json:{list:{path:[listel,listlel,listel]}}}
+ */
 function initialize(option, el) {
   $.getJSON( option["source"], function( data ) {
 
@@ -49,6 +48,59 @@ function initialize(option, el) {
     }); // end data loop
   }); // end ajax request
 } // end init function
+
+
+ /*    Allowed options:
+ *      "keywords": Keywords, divided by spaces
+ *        e.g. "javascript php something"
+ *      "hideUnrelevants": If true, list elements not matching the search keywords
+ *        will be hidden
+ *      "caseSensitive": If true, it will matter wether keywords are lower or
+ *        uppercase
+ */
+function search(option, el) {
+  var defaults = {
+    "keywords": option["keywords"] || "",
+    "hideUnrelevants": option["hideUnrelevants"] || "true",
+    "caseSensitive": option["caseSensitive"] || "false"
+  }
+
+  var option = $.extend({}, defaults, option);
+
+  $el = $(el)
+    .find(".sl-element")
+    .removeClass("searchFound")
+    .removeClass("searchNotFound")
+    .each(function() {
+      matches = 0;
+
+      $(this)
+        .find("[data-value]")
+        .each(function() {
+          // iterate over value elements in listelement
+          var valelement = $(this);
+          $.each(option["keywords"].split(" "), function(i, keyword) {
+            // iterate over list of keywords
+            thisMatches = valelement.html().match(option["caseSensitive"] == "true" ? keyword : new RegExp(keyword, "i"));
+            matches += thisMatches == null ? 0 : thisMatches.length;
+          });
+        });
+
+      if(matches != 0) {
+        $(this).addClass("searchFound");
+      } else {
+        $(this).addClass("searchNotFound");
+      }
+    });
+
+  $(el).find(".sl-element.searchFound").css("display", "block");
+
+  if(option["hideUnrelevants"] == "true") {
+    $(el).find(".sl-element.searchNotFound").css("display", "none");
+  } else {
+    $(el).find(".sl-element.searchNotFound").css("display", "block");
+  }
+}
 
 $(document).ready(function() {
   $("<style type='text/css'>.sl-prototype-element {display: none}</style>").appendTo("head");
