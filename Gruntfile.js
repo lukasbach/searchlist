039a204738c1a16da3d2a53f1c68a7c3307eb04b
Lukas Bach <lbach@outlook.de>
2015-09-19 14:41:29 +0200

    Changed Gruntfile, added uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,35 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
       options: {
-        separator: '',
+        separator: '\n\n',
       },
       dist: {
-        src: ['src/core/*.js'],
+        src: ['src/core/private.js', 'src/core/grouping.js', 'src/core/inteface.js', 
+        'src/core/listmanipulation.js', 'src/core/search.js', 'src/core/sorting.js', 
+        'src/core/transforming.js', 'src/core/init.js'],
         dest: 'dist/searchlist.js',
       },
     },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'dist/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
